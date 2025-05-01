11206030702b6f7fdadfa2bebabaa9905fcccb7c
zihuaxin <764718384xzh@gmail.com>
2025-05-01 00:11:54 +0000

    latest training script

--- train.sh
+++ train.sh
@@ -1,13 +1,12 @@
 configs=(
-    "h5_w5_c3_large.yaml"
-    "h6_w7_c4_small.yaml"
+    "h6_w7_c4_small_200.yaml"
     "h6_w7_c4_large.yaml"
     "h5_w5_c3_small.yaml"
-    "h5_w5_c3_medium.yaml" 
     "h6_w7_c4_medium.yaml"
     "h7_w8_c5_small_200.yaml"
     "h7_w8_c5_small_400.yaml"
     "h7_w8_c5_small_600.yaml"
+    "h9_w9_c5_small.yaml"
 )
 
 for config in "${configs[@]}"; do
